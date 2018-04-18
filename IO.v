8e13d5d2d708f8b9dc3b2be893828f5d14293e51
Isaiah Grace <igrace@purdue.edu>
2018-04-17 20:04:02 -0400

    fixed typo

--- IO.v
+++ IO.v
@@ -5,6 +5,6 @@ module IO (IOR, IOW, DATA, INPUT, OUTPUT);
    input wire [15:0]  INPUT;
    output wire [15:0] OUTPUT;
 
-   assign DATA   = ( IOR & !IOW) ? INPUT : 16'bzzzz zzzz zzzz zzzz;
-   assign OUTPUT = (!IOR &  IOW) ? DATA  : 16'bzzzz zzzz zzzz zzzz;
+   assign DATA   = ( IOR & !IOW) ? INPUT : 16'bzzzzzzzzzzzzzzzz;
+   assign OUTPUT = (!IOR &  IOW) ? DATA  : 16'bzzzzzzzzzzzzzzzz;
 endmodule // IO
